c07a097b8a2b04ce7cff6d362775b0fc33be3b97
PandaFuKen <zaletagames@hotmail.com>
2025-05-20 11:49:17 -0600

    cambios

--- routes/index.js
+++ routes/index.js
@@ -47,7 +47,7 @@ router.get('/logout', (req, res) => {
       console.error('Error al cerrar sesión:', err);
       return res.status(500).send('Error al cerrar sesión');
     }
-    res.redirect('/login');
+    res.redirect('/');
   });
 });
 
